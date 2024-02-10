9371d29930c41a718abfda2a73cac18ef54a192a
wesley <wesleywong08@gmail.com>
2024-02-11 02:33:33 +0800

    Remove old files

--- backend/config/initializers/session_store.rb
+++
@@ -1,9 +0,0 @@
-# initialize the session store since api only not loaded
-
-Rails.application.config.middleware.use ActionDispatch::Cookies
-Rails.application.config.middleware.use ActionDispatch::Session::CookieStore, key: "_auth"
-Rails.application.config.middleware.insert_after(
-  ActionDispatch::Cookies,
-  ActionDispatch::Session::CookieStore,
-  key: "_auth",
-)
